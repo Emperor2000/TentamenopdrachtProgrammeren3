cf470733df256f96e90842caa20a2e4acc8f1eb5
Vincent <vincenthendriks2000@gmail.com>
2019-02-09 20:29:38 +0100

    Added a new activity infoActivity, should be child activity of activity main.

---
+++ RandomPersonApp/app/src/main/java/com/example/randomuserapp/presentation/InfoActivity.java
@@ -0,0 +1,15 @@
+package com.example.randomuserapp.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.randomuserapp.R;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
