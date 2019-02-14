136cc71410714e26b9e9a326f05e09621a286a58
Vincent <vincenthendriks2000@gmail.com>
2019-02-14 16:24:05 +0100

    Added lifecycle events, added in MainActivity. Added a new tag named cycleTag

--- RandomPersonApp/app/src/main/java/com/example/randomuserapp/presentation/MainActivity.java
+++ RandomPersonApp/app/src/main/java/com/example/randomuserapp/presentation/MainActivity.java
@@ -24,7 +24,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements JSONService.JSONServiceListener {
-
+    private final String cycleTag = "LifeCycleEvents";
     private final String TAG = MainActivity.class.getSimpleName();
     private RecyclerView recyclerView;
     private ArrayList<Profile> profileList = new ArrayList<>();
@@ -76,5 +76,39 @@ public class MainActivity extends AppCompatActivity
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         Toast.makeText(this, "Changed orientation", Toast.LENGTH_LONG).show();
+    }
+
+
+
+
+    public void onStart()
+    {
+        super.onStart();
+        Log.d(cycleTag, "In the onStart() event");
+    }
+    public void onRestart()
+    {
+        super.onRestart();
+        Log.d(cycleTag, "In the onRestart() event");
+    }
+    public void onResume()
+    {
+        super.onResume();
+        Log.d(cycleTag, "In the onResume() event");
+    }
+    public void onPause()
+    {
+        super.onPause();
+        Log.d(cycleTag, "In the onPause() event");
+    }
+    public void onStop()
+    {
+        super.onStop();
+        Log.d(cycleTag, "In the onStop() event");
+    }
+    public void onDestroy()
+    {
+        super.onDestroy();
+        Log.d(cycleTag, "In the onDestroy() event");
     }
 }
